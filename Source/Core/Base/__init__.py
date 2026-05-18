50753cfbe59ddf90e725a7d7ceb6adba72c573de
DUB1401 <vlad.milosta@yandex.ru>
2026-05-18 11:04:19 +0300

    Clear legacy

--- Source/Core/Base/__init__.py
+++ Source/Core/Base/__init__.py
@@ -1,4 +1,4 @@
-from .Binder import Binder
+from ....Legacy.Binder import Binder
 from .Manifest import Manifest
-from .Objects import Table, Module, Note
+from ....Legacy.Objects import Table, Module, Note
 from .Structs import Attachments, ModuleData
--- Source/Tables/__init__.py
+++ Source/Tables/__init__.py
@@ -1,6 +1,6 @@
-from Source.Tables.Otaku import Otaku
-from Source.Tables.Otaku.Anime import Otaku_Anime
+from Legacy.Otaku import Otaku
+from Legacy.Otaku.Anime import Otaku_Anime
 
-from Source.Tables.BattleTech import BattleTech
-from Source.Tables.BattleTech.Books import BattleTech_Books
-from Source.Tables.BattleTech.Sources import BattleTech_Sources
+from Legacy.BattleTech import BattleTech
+from Legacy.BattleTech.Books import BattleTech_Books
+from Legacy.BattleTech.Sources import BattleTech_Sources
